29e2d33a5cb96ce1dc32e5d64ec4b2ae5a172d5f
dstrockis <dastrock@microsoft.com>
2015-09-14 15:01:19 -0700

    Forgot to change default clientId

--- WebApp-B2C-DotNet/Scripts/default.js
+++ WebApp-B2C-DotNet/Scripts/default.js
@@ -1,6 +1,6 @@
 ﻿function fillDefault() {
     $("#tenant").val("fabrikamb2c.onmicrosoft.com");
-    $("#client_id").val("79467a70-1adc-41a2-9d0a-faebefb5866c");
+    $("#client_id").val("90c0fe63-bcf2-44d5-8fb7-b8bbc0b29dc6");
     $("#sign_in_policy").val("b2c_1_sign_in");
     $("#sign_up_policy").val("b2c_1_sign_up");
     $("#edit_profile_policy").val("b2c_1_edit_profile");
